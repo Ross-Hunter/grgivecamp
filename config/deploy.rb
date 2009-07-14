f68a4cd6ebd054203c356fd9fdd446b0600cc2f7
John Hwang <jhwang@mutuallyhuman.com>
2009-07-13 20:03:10 -0400

    updating capistrano deploy scripts

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,56 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "grgivecamp"
+set :user, "grgivecamp"
+set :username, "grgivecamp"
+set :repository, "ssh://grgivecamp@grgivecamp.org/home/grgivecamp/git/grgivecamp.org" 
+set :deploy_to, "/home/grgivecamp/www/grgivecamp.org"
+set :use_sudo, false
+set :assets, %W(sites/default/files sites/default/settings.php)
 
-# If you aren't deploying to /u/apps/#{application} on the target
-# servers (which is the default), you can specify the actual location
-# via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+role :web, "grgivecamp.org"
+role :db, "grgivecamp.org"
 
-# If you aren't using Subversion to manage your source code, specify
-# your SCM below:
-# set :scm, :subversion
+namespace :drupal do
+  task :symlink, :except => { :no_release => true } do
+    logger.info 'Symlinking assets'
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+    assets.each do |asset|
+      run "ln -s #{shared_path}/#{asset} #{release_path}/#{asset}"
+    end
+  end  
+end
+after 'deploy:symlink', 'drupal:symlink'
+
+namespace :db do
+  namespace :production do
+    desc "Pull Production Database"
+    task :pull, :roles => :db do
+      require "yaml"
+      get "#{shared_path}/sites/default/database.yml", "/tmp/database.yml"
+      db_settings = YAML.load_file("/tmp/database.yml")
+      database = db_settings["production"]["database"]      
+      username = db_settings["production"]["username"]
+      password = db_settings["production"]["password"]
+      host = db_settings["production"]["host"]
+    
+      filename = "#{database}-#{Time.now.strftime '%Y%m%d%H%M%S'}.dump"
+
+      on_rollback { 
+        run "rm /tmp/#{filename}"
+        run "rm /tmp/#{filename}.gz"
+        run "rm /tmp/database.yml"
+      }
+      
+      run "mysqldump -u#{username} -p'#{password}' -h#{host} #{database} > /tmp/#{filename}"
+      run "gzip /tmp/#{filename}"
+      get "/tmp/#{filename}.gz", "/tmp/#{filename}.gz"
+      run "rm /tmp/#{filename}.gz"
+
+      system "gunzip /tmp/#{filename}.gz"
+      system "mysqladmin -f -uroot drop #{application}_development"
+      system "mysqladmin -f -uroot create #{application}_development"
+      system "mysql -uroot #{application}_development < /tmp/#{filename}"
+      system "rm /tmp/#{filename}"
+      system "rm /tmp/database.yml"      
+    end
+  end
+end
