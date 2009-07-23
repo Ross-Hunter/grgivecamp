d3ad21432790b796c9cf6a32be6ed527ac0a9b02
John Hwang <jhwang@mutuallyhuman.com>
2009-07-23 11:23:21 -0400

    Add sample code and graphic for homepage feature

--- sites/default/themes/grgivecamp/page-front.tpl.php
+++ sites/default/themes/grgivecamp/page-front.tpl.php
@@ -2,7 +2,38 @@
   include('header.tpl.php');
 ?>
 
-<div class="main container_12 clearfix">
+<div class="feature_wrapper clearfix">
+  <div class="feature tab1 container_12 clearfix">
+    <div class="tabs clearfix">
+      <div class="tab non-profit tab1 grid_12 clearfix">
+        <div class="content">
+          <h3>Non-Profit &amp; Charities</h3>
+          <p class="grid_5">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+          <div class="links grid_6">
+            <a href="" class="button">Submit Application</a> or <a href="">find out how we can help</a>
+          </div>
+        </div>
+      </div>
+      <!-- <div class="tab tab2 grid_12 clearfix">
+    
+      </div>
+      <div class="tab tab3 grid_12 clearfix">
+    
+      </div>
+      <div class="tab tab4 grid_12 clearfix">
+    
+      </div>     -->
+    </div>
+    <ul class="clearfix">
+      <li class="grid_3 non-profit tab1"><a href="/2009/non-profits-and-charities">Non-Profit</a></li>
+      <li class="grid_3 tab2"><a href="/2009/volunteer">Volunteer</a></li>
+      <li class="grid_3 tab3"><a href="/2009/helper">Helper</a></li>
+      <li class="grid_3 tab4"><a href="/2009/sponsor">Sponsor</a></li>
+    </ul>
+  </div>
+</div>
+
+<div class="main container_12 clearfix">  
   <div class="grid_12">
     <?php print $content ?>
   </div>
