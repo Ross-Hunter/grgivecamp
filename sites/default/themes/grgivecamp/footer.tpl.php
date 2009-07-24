91be0ccae1a9b4f62642a6202bf626618d4a88aa
John Hwang <jhwang@mutuallyhuman.com>
2009-07-24 10:32:59 -0400

    * Fixing givecamp copy. * Fixing wrong link

--- sites/default/themes/grgivecamp/footer.tpl.php
+++ sites/default/themes/grgivecamp/footer.tpl.php
@@ -9,14 +9,14 @@
       <a href="/about">About</a>
       <ul class="sub_menu">
         <li><a href="/about/faq">Frequently Asked Questions</li>
-        <li><a href="/about/new">News</li>
+        <li><a href="/about/news">News</li>
         <li><a href="/about/media">Media</li>
         <li><a href="/about/other">Other Give Camps</li>
         <li><a href="mailto:info@grgivecamp.org">Contact Us</li>
       </ul>
     </li>
     <li class="primary_menu grid_3">
-      <a href="/2009">Give Camp 2009</a>
+      <a href="/2009">GiveCamp 2009</a>
       <ul class="sub_menu">
         <li><a href="/2009/get-involved">Get Involved</a></li>
         <li><a href="/2009/non-profits-and-charities">Non-Profits/Charities</a></li>
