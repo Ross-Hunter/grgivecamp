f880c031a6f5d3cf7febec11d11130a68d2a0ecf
John Hwang <jhwang@mutuallyhuman.com>
2009-07-26 23:36:10 -0400

    Updating homepage feature copy per Emily's updates

--- sites/default/themes/grgivecamp/page-front.tpl.php
+++ sites/default/themes/grgivecamp/page-front.tpl.php
@@ -5,17 +5,18 @@
     <div class="tabs clearfix">
       <div class="tab non-profit tab1 grid_12 clearfix">
         <div class="content">
-          <h3>Non-Profit &amp; Charities <span>(Deadline Sept 1st)</span></h3>
-          <p class="grid_8">You're already too busy as is.  You know you need to update your website but you've pushed it off because your time and resources are limited and you need to care for your constituents first.  We're here to help.  We're gathering 100+ technical geniuses to help your organization do what it does best and not worry about the technical stuff.  Hurry! Space is limited!</p>
+          <h3>Non-Profit <span>(Deadline Sept 1st)</span></h3>
+          <p class="grid_8">Does your non-profit need a new website? Time to escape those Excel spreadsheets and get a real donor database? Trying to figure out how to build your social media presence? Give Camp is for you.</p>
+          <p class="grid_8">Join us for a weekend-long volunteer event that brings together nearly 100 technology professionals committed to providing your organization with solutions, strategies, and tools that can help you continue your good work.</p>
           <div class="links grid_6">
-            <a href="" class="button">Submit an application</a> or <a href="">find out how we can help</a>
+            <a href="" class="button">Apply Now</a> or <a href="">find out how we can help</a>
           </div>
         </div>
       </div>
       <div class="tab volunteer tab2 grid_12 clearfix">
         <div class="content">
           <h3>Technical Volunteer</h3>
-          <p class="grid_8">Ever wanted to give back to the community using the talents and skills you've acquired over the years?  Here's your chance.  We are looking for graphic or UX designers, developers with .NET/Java/LAMP/Open Source/CMS experience, project managers, system/network administrators, DBAs, non-profit consultants/strategists to get together and donate their time and expertise to non-profits and charities.  Plus there's free food and multiplayer Rock Band every night.</p>
+          <p class="grid_8">Ready to connect with the local tech community for a good cause? Give Camp needs your support and skills. For one weekend in November, we’ll take on the technology wish lists of 20 non-profit organizations. We’re looking for a variety of volunteers: graphic or UX designers, developers with .NET/Java/LAMP/Open Source/CMS experience, project managers, system/network administrators, DBAs, and non-profit technical consultants and web strategists.</p>
           <div class="links grid_6">
             <a href="" class="button">Signup to Volunteer</a> or <a href="">find out how you can help</a>
           </div>
@@ -23,8 +24,8 @@
       </div>
       <div class="tab helper tab3 grid_12 clearfix">
         <div class="content">
-          <h3>Helper Volunteer</h3>
-          <p class="grid_8">Give Camp is not just about artist, geeks and techies hacking in a dark room.  We need your support in running a successful charity camp. We are looking for self-less, amazing, wonderful people to help us organize, plan, and ultimately herd the geeks together and protect them from the elements.</p>
+          <h3>Non-Technical Volunteer</h3>
+          <p class="grid_8">It’s not just about the technology – there are a lot of other aspects that help Give Camp run smoothly. We need non-technical volunteers to run errands, help with setting up and cleaning up, and everything in between. Whether you can offer a couple hours of your time or join Give Camp for a couple days, your support is needed!</p>
           <div class="links grid_6">
             <a href="" class="button">Signup to Volunteer</a> or <a href="">find out how you can help</a>
           </div>
@@ -32,18 +33,18 @@
       </div>
       <div class="tab sponsor tab4 grid_12 clearfix">
         <div class="content">
-          <h3>Sponsor GiveCamp</h3>
-          <p class="grid_8">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+          <h3>Sponsor</h3>
+          <p class="grid_8">As a Give Camp sponsor, you help connect the talent of local technology professionals with the needs of local non-profit organizations. In the course of one weekend, almost 20 projects will be completed at no cost to participating organizations. There are many ways to support this work – become a sponsor and ensure Give Camp is a continued success.</p>
           <div class="links grid_6">
-            <a href="" class="button">Sponsor GiveCamp</a> or <a href="">find out more about GiveCamp</a>
+            <a href="" class="button">Become a sponsor</a> or <a href="">find out more about GiveCamp</a>
           </div>
         </div>    
       </div>    
     </div>
     <ul class="clearfix">
       <li class="grid_3 tab1"><a href="/2009/non-profits-and-charities">Non-Profit</a></li>
-      <li class="grid_3 tab2"><a href="/2009/volunteer">Volunteer</a></li>
-      <li class="grid_3 tab3"><a href="/2009/helper">Helper</a></li>
+      <li class="grid_3 tab2"><a href="/2009/volunteer">Technical Volunteer</a></li>
+      <li class="grid_3 tab3"><a href="/2009/helper">Non-Technical Volunteer</a></li>
       <li class="grid_3 tab4"><a href="/2009/sponsor">Sponsor</a></li>
     </ul>
   </div>
