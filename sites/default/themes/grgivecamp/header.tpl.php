9e417ee35730e3a407e0ffd017fc0057320e53ca
John Hwang <jhwang@mutuallyhuman.com>
2009-07-29 12:22:05 -0400

    commenting out links that are ready

--- sites/default/themes/grgivecamp/header.tpl.php
+++ sites/default/themes/grgivecamp/header.tpl.php
@@ -98,7 +98,7 @@
           <dd>Find out more about the awesome individuals who are participating in GiveCamp 2009.</dd>
         </dl>
       </div>      
-      <div class="link">
+      <!-- <div class="link">
         <dl class="clearfix">      
           <dt><a href="/2009/schedule">Schedule</a></dt>
           <dd>Find out more about the details of activities and scheduled events during the three day camp.</dd>
@@ -109,7 +109,7 @@
           <dt><a href="/2009/venue">Venue</a></dt>
           <dd>Learn where GiveCamp 2009 will be held this year.</dd>
         </dl>
-      </div>            
+      </div>             -->
     </div>    
     
     <?php if(isset($secondary_links)): ?>
