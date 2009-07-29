45a15224b8166014f843e0c2c985282c62d3c4ae
John Hwang <jhwang@mutuallyhuman.com>
2009-07-29 12:31:49 -0400

    Moving content out of footer

--- sites/default/themes/grgivecamp/footer.tpl.php
+++ sites/default/themes/grgivecamp/footer.tpl.php
@@ -1,38 +1,27 @@
 <div class="footer container_12 clearfix">
-  <div class="givecamp grid_3">
-    <a href="http://givecamp.org" class="givecamp">National GiveCamp</a>
-    <a href="http://michigangivecamp.org/" class="michgivecamp">Michigan GiveCamp</a>
+  <div class="grid_6">
+    <p><?php print $footer_message ?></p>
   </div>
-
-  <ul class="">
-    <li class="primary_menu grid_3">
-      <a href="/about">About</a>
-      <ul class="sub_menu">
-        <li><a href="/about/faq">Frequently Asked Questions</li>
-        <li><a href="/about/news">Recent News</li>
-        <li><a href="/about/media">Media</li>
-        <li><a href="/about/other">Other GiveCamps</li>
-        <li><a href="mailto:info@grgivecamp.org">Contact Us</li>
-      </ul>
-    </li>
-    <li class="primary_menu grid_3">
-      <a href="/2009">GiveCamp 2009</a>
-      <ul class="sub_menu">
-        <li><a href="/2009/get-involved">Get Involved</a></li>
-        <li><a href="/2009/non-profits">Non-Profits</a></li>
-        <li><a href="/2009/sponsors">Sponsors</a></li>
-        <li><a href="/2009/participants">Participants</a></li>
-        <li><a href="/2009/schedule">Schedule</a></li>
-        <li><a href="/2009/venue">Venue</a></li>
-      </ul>
-    </li>      
-  </ul>
-  
-  <p class="grid_3">
-    <?php print $footer ?>
-  </p>
-  
-  <p class="grid_12">
-    <?php print $footer_message ?>
-  </p>
+    
+  <div class="primary_menu grid_3">
+    <h4><a href="/about">About</a></h4>
+    <ul class="sub_menu">
+      <li><a href="/about/faq">Frequently Asked Questions</li>
+      <li><a href="/about/news">Recent News</li>
+      <li><a href="/about/media">Media</li>
+      <li><a href="/about/other">Other GiveCamps</li>
+      <li><a href="mailto:info@grgivecamp.org">Contact Us</li>
+    </ul>
+  </div>
+  <div class="primary_menu grid_3">
+    <h4><a href="/2009">GiveCamp 2009</a></h4>
+    <ul class="sub_menu">
+      <li><a href="/2009/get-involved">Get Involved</a></li>
+      <li><a href="/2009/non-profits">Non-Profits</a></li>
+      <li><a href="/2009/sponsors">Sponsors</a></li>
+      <li><a href="/2009/participants">Participants</a></li>
+      <li><a href="/2009/schedule">Schedule</a></li>
+      <li><a href="/2009/venue">Venue</a></li>
+    </ul>
+  </div>
 </div>
