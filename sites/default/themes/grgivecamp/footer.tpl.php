07f6d6009e483989c35304b5ffa5552a04d89880
John Hwang <jhwang@mutuallyhuman.com>
2009-07-29 12:34:57 -0400

    commenting out links

--- sites/default/themes/grgivecamp/footer.tpl.php
+++ sites/default/themes/grgivecamp/footer.tpl.php
@@ -20,8 +20,8 @@
       <li><a href="/2009/non-profits">Non-Profits</a></li>
       <li><a href="/2009/sponsors">Sponsors</a></li>
       <li><a href="/2009/participants">Participants</a></li>
-      <li><a href="/2009/schedule">Schedule</a></li>
-      <li><a href="/2009/venue">Venue</a></li>
+      <!-- <li><a href="/2009/schedule">Schedule</a></li> -->
+      <!-- <li><a href="/2009/venue">Venue</a></li> -->
     </ul>
   </div>
 </div>
